bcaeffa2415d8158ad4f697d2ebb0097ba4d9e93
Tsutomu Funada <forestlaw.me+git@gmail.com>
2025-08-17 11:41:22 +0000

    react-responsive という類似コンポーネントを見つけたので screenHelper.tsx を削除

--- components/common/DynamicBreadcrumb.tsx
+++ components/common/DynamicBreadcrumb.tsx
@@ -1,11 +1,9 @@
 /**
+ * @copyright Copyright (c) 2025 Tsutomu FUNADA
  * @license
- * Copyright (c) 2025 Tsutomu FUNADA
- *
  * This software is licensed for:
  * - Non-commercial use under the MIT License (see LICENSE-NC.txt)
  * - Commercial use requires a separate commercial license (contact author)
- *
  * You may not use this software for commercial purposes under the MIT License.
  */
 
